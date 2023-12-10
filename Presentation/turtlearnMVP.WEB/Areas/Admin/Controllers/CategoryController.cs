45d7a1e612b3d8033d8054ae52ce53538646e2d7
UtariD86 <69503659+UtariD86@users.noreply.github.com>
2023-12-10 03:02:27 +0300

    category index genel bir görünüme kavuştu. jqgrid temasyla beraber dahil edildi -utarid

--- Presentation/turtlearnMVP.WEB/Areas/Admin/Controllers/CategoryController.cs
+++ Presentation/turtlearnMVP.WEB/Areas/Admin/Controllers/CategoryController.cs
@@ -2,11 +2,32 @@
 
 namespace turtlearnMVP.WEB.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class CategoryController : Controller
     {
         public IActionResult Index()
         {
             return View();
         }
+
+        public ActionResult GetCategories()
+        {
+            // Sample data
+            var categories = new List<CategoryViewModel>
+        {
+            new CategoryViewModel { Id = 1, Name = "AAAAA" },
+            new CategoryViewModel { Id = 2, Name = "BBBBB" },
+            new CategoryViewModel { Id = 3, Name = "CCCCC" }
+            // Add more departments as needed
+        };
+
+            return Json(categories);
+        }
+    }
+
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
     }
 }
