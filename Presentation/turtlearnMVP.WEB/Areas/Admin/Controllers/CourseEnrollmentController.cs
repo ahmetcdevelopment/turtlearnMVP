ce86e419b4ea1ec2125256e116d859ed17f30ec4
ahmet çiftçi <ciftciahmet70@hotmail.com>
2023-12-20 19:26:43 +0300

    mal memet xdd

---
+++ Presentation/turtlearnMVP.WEB/Areas/Admin/Controllers/CourseEnrollmentController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using turtlearnMVP.Application.Persistance.Services;
+
+namespace turtlearnMVP.WEB.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CourseEnrollmentController : Controller
+    {
+        private readonly ICourseEnrollmentService _courseEnrollmentService;
+        private readonly ICourseService _courseService;
+
+        public CourseEnrollmentController(ICourseEnrollmentService courseEnrollmentService, ICourseService courseService)
+        {
+            _courseEnrollmentService = courseEnrollmentService;
+            _courseService = courseService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
