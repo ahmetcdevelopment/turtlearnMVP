348b7ed92f78cbd32c47d5c81e0f26daf8d234c0
UtariD86 <69503659+UtariD86@users.noreply.github.com>
2024-01-29 06:46:46 +0300

    error sayfası tamamlandı. profil sayfası kabaca tamamlandı. usersettings ve edit profil syfasına dahil edilecek. -utarid

--- Presentation/turtlearnMVP.WEB/Models/ErrorViewModel.cs
+++ Presentation/turtlearnMVP.WEB/Models/ErrorViewModel.cs
@@ -3,7 +3,13 @@ namespace turtlearnMVP.WEB.Models
     public class ErrorViewModel
     {
         public string? RequestId { get; set; }
+        public int? ErrorCode { get; set; } // Hata kodunu burada tutuyoruz.
+        public string? ErrorMessage { get; set; } // Hata kodunu burada tutuyoruz.
+        public string? ErrorDescription { get; set; } // Hata kodunu burada tutuyoruz.
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+        public bool ShowErrorCode => ErrorCode.HasValue; // Hata kodu varsa göster.
+        public bool ShowErrorDescription => ErrorCode.HasValue; // Hata kodu varsa göster.
+        public bool ShowErrorMessage => ErrorCode.HasValue; // Hata kodu varsa göster.
     }
 }
